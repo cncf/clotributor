35826c494ed3ce6f698e256089ff7ac7c710cde5
Cintia Sánchez García <cynthiasg@icloud.com>
2022-11-25 12:24:36 +0100

    Make labels in issue card clickable (#39)
    Closes #34
    Closes #35
    
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- web/clo-ui/src/components/GenericBadge/GenericBadge.tsx
+++ web/clo-ui/src/components/GenericBadge/GenericBadge.tsx
@@ -1,3 +1,4 @@
+import { isUndefined } from 'lodash';
 import React from 'react';
 
 import styles from './GenericBadge.module.css';
@@ -5,12 +6,25 @@ import styles from './GenericBadge.module.css';
 export interface IGenericBadgeProps {
   content: string;
   className?: string;
+  onClick?: () => void;
 }
 
 export const GenericBadge: React.FC<IGenericBadgeProps> = (props: IGenericBadgeProps) => {
   return (
-    <div className={`badge fw-normal rounded-0 position-relative ${styles.outlineBadge} ${props.className}`}>
-      {props.content}
-    </div>
+    <>
+      {isUndefined(props.onClick) ? (
+        <div className={`badge fw-normal rounded-0 position-relative ${styles.outlineBadge} ${props.className}`}>
+          {props.content}
+        </div>
+      ) : (
+        <button
+          type="button"
+          className={`btn btn-sm badge fw-normal rounded-0 position-relative clickable ${styles.outlineBadge} ${styles.clickable} ${props.className}`}
+          onClick={props.onClick}
+        >
+          {props.content}
+        </button>
+      )}
+    </>
   );
 };
