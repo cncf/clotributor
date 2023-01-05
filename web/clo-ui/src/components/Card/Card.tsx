799b5b518a9e81257e46801f4e821b26c4f6f207
Cintia Sánchez García <cynthiasg@icloud.com>
2023-01-05 11:48:40 +0100

    Improve dark theme (#117)
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- web/clo-ui/src/components/Card/Card.tsx
+++ web/clo-ui/src/components/Card/Card.tsx
@@ -4,18 +4,23 @@ import React from 'react';
 import styles from './Card.module.css';
 
 export interface ICardProps {
-  wrapperClassname?: string;
+  wrapperClassName?: string;
+  className?: string;
   children: JSX.Element;
   hoverable: boolean;
 }
 
 export const Card: React.FC<ICardProps> = (props: ICardProps) => {
   return (
-    <div className={props.wrapperClassname} role="listitem">
+    <div className={props.wrapperClassName} role="listitem">
       <div
-        className={classNames('card rounded-0 p-0 h-100 mw-100 d-flex text-reset text-decoration-none card', {
-          [styles.hoverable]: props.hoverable,
-        })}
+        className={classNames(
+          'card rounded-0 p-0 h-100 mw-100 d-flex text-reset text-decoration-none',
+          props.className,
+          {
+            [styles.hoverable]: props.hoverable,
+          }
+        )}
       >
         {props.children}
       </div>
